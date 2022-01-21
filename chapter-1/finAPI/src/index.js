5c3dba237f93ed4de9e664f19e60abb2121f0bec
Note45 <antonion.2048@gmail.com>
2022-01-21 08:57:15 -0300

    add routes create custumer and get statement

--- chapter-1/finAPI/src/index.js
+++ chapter-1/finAPI/src/index.js
@@ -1,5 +1,40 @@
+const { application } = require("express");
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (custumer) => custumer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
+
+  customers.push({
+    id: uuidv4(),
+    cpf,
+    name,
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  return response.json(customer.statement);
+});
 
 app.listen(3333);
