afbae0f789e636226c40b2482e3e52720c90c662
Note45 <antonion.2048@gmail.com>
2022-01-23 16:34:26 -0300

    add withdraw route to finAPI

--- chapter-1/finAPI/src/index.js
+++ chapter-1/finAPI/src/index.js
@@ -23,6 +23,19 @@ const verifyIfExistsAccountCPF = (request, response, next) => {
   next();
 };
 
+//helpers
+const getBalance = (statement) => {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+};
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -59,6 +72,27 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
     amount,
     createdAt: new Date(),
     type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insuficient funds!" });
+  }
+
+  const statementOperation = {
+    amount,
+    createdAt: new Date(),
+    type: "debit",
   };
 
   customer.statement.push(statementOperation);
