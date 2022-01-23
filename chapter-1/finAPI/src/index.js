0617ebb39594c2f06852336f111ed69c1b2d592f
Note45 <antonion.2048@gmail.com>
2022-01-23 16:04:06 -0300

    add deposit route to finAPI

--- chapter-1/finAPI/src/index.js
+++ chapter-1/finAPI/src/index.js
@@ -8,6 +8,21 @@ app.use(express.json());
 
 const customers = [];
 
+//Middleware
+const verifyIfExistsAccountCPF = (request, response, next) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found!" });
+  }
+
+  request.customer = customer;
+
+  next();
+};
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -29,16 +44,26 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found!" });
-  }
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
 
   return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    createdAt: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
