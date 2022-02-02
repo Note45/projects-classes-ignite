8e270757fe5c2e9870fff80f2e8460daeb97eb96
note45 <antonion.2048@gmail.com>
2022-02-02 09:25:18 -0300

    fix eslint not working

--- chapter-2/rentalx/src/routes/categories.routes.ts
+++ chapter-2/rentalx/src/routes/categories.routes.ts
@@ -1,18 +1,18 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post("/categories", (request, response) => {
   const { name, description } = request.body;
 
   categories.push({
     name,
-    description
+    description,
   });
 
   return response.status(201).send();
-})
+});
 
 export { categoriesRoutes };
