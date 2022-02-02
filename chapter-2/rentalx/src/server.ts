028b31522d80ac07c271b500573ad94ec25ce7ff
note45 <antonion.2048@gmail.com>
2022-02-02 09:07:10 -0300

    add vscode debug config to project

--- chapter-2/rentalx/src/server.ts
+++ chapter-2/rentalx/src/server.ts
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello world!' })
+  return response.json({ message: 'Hello world!' });
+})
+
+app.post('/coursers', (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
 })
 
 app.listen(3333, () => console.log("Server start at port 3333!"));
