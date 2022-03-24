fd4dc0fbb1ed787ce393cf306aef49cc9dd4f414
Note45 <antonion.2048@gmail.com>
2022-03-24 19:12:35 -0300

    cut old rentalx folder

--- chapter-2/rentalx/src/routes/specification.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
-
-export const specificationRoutes = Router();
-
-specificationRoutes.post("/", (request, response) => {
-  return createSpecificationController.handle(request, response);
-});
