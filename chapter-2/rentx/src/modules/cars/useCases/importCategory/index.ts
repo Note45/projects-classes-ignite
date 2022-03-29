a6278ab23506615456e0e5abcdc19500da658282
Note45 <antonion.2048@gmail.com>
2022-03-29 07:47:03 -0300

    add logic to use repository with typeorm in use cases

--- chapter-2/rentx/src/modules/cars/useCases/importCategory/index.ts
+++ chapter-2/rentx/src/modules/cars/useCases/importCategory/index.ts
@@ -2,12 +2,14 @@ import { CategoriesRepository } from "../../repositories/implementations/Categor
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const categoriesRepository = CategoriesRepository.getInstance();
+export default () => {
+  const categoriesRepository = new CategoriesRepository();
 
-export const importCategoryUseCase = new ImportCategoryUseCase(
-  categoriesRepository
-);
+  const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 
-export const importCategoryController = new ImportCategoryController(
-  importCategoryUseCase
-);
+  const importCategoryController = new ImportCategoryController(
+    importCategoryUseCase
+  );
+
+  return importCategoryController;
+};
