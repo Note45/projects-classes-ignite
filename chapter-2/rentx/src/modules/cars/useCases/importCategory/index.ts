c9c606b4487a75ddfc3360a715fba9ca634767b2
Note45 <antonion.2048@gmail.com>
2022-03-30 08:55:54 -0300

    add type return index use case import category

--- chapter-2/rentx/src/modules/cars/useCases/importCategory/index.ts
+++ chapter-2/rentx/src/modules/cars/useCases/importCategory/index.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from "../../repositories/implementations/Categor
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-export default () => {
+export default (): ImportCategoryController => {
   const categoriesRepository = new CategoriesRepository();
 
   const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
