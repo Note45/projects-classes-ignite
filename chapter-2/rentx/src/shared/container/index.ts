1fc2c86af567f7b324b2a8d7d539ebfc2343f360
Note45 <antonion.2048@gmail.com>
2022-04-03 10:51:58 -0300

    refactor use cases to use repository with db postgress

--- chapter-2/rentx/src/shared/container/index.ts
+++ chapter-2/rentx/src/shared/container/index.ts
@@ -2,8 +2,14 @@ import { container } from "tsyringe";
 
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   CategoriesRepository
+);
+
+container.registerSingleton<SpecificationRepository>(
+  "SpecificationRepository",
+  SpecificationRepository
 );
