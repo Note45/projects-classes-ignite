b5905b5441d2719b2c9c0f93e88a5a3a8ec51af4
Note45 <antonion.2048@gmail.com>
2022-04-04 08:26:26 -0300

    refactor SpecificationRepository to use typeorm

--- chapter-2/rentx/src/modules/cars/repositories/implementations/SpecificationRepository.ts
+++ chapter-2/rentx/src/modules/cars/repositories/implementations/SpecificationRepository.ts
@@ -1,3 +1,5 @@
+import { getRepository, Repository } from "typeorm";
+
 import { Specification } from "../../entities/Specification";
 import {
   ICreateSpecificationDTO,
@@ -5,28 +7,25 @@ import {
 } from "../ISpecificationsRepository";
 
 export class SpecificationRepository implements ISpecificationsRepository {
-  private specifications: Specification[];
+  private repository: Repository<Specification>;
 
   constructor() {
-    this.specifications = [];
+    this.repository = getRepository(Specification);
   }
 
-  create({ name, description }: ICreateSpecificationDTO): void {
-    const specification = new Specification();
-
-    Object.assign(specification, {
+  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+    const specification = this.repository.create({
       name,
       description,
-      created_at: new Date(),
     });
 
-    this.specifications.push(specification);
+    await this.repository.save(specification);
   }
 
-  findByName(name: string): Specification {
-    const category = this.specifications.find(
-      (specification) => specification.name === name
-    );
+  async findByName(name: string): Promise<Specification> {
+    const category = await this.repository.findOne({
+      name,
+    });
 
     return category;
   }
