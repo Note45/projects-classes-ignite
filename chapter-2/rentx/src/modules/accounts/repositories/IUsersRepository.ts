c662094f62fcec10372721e8651178b01ac28584
Note45 <antonion.2048@gmail.com>
2022-04-10 15:36:31 -0300

    add logic to hash user password

--- chapter-2/rentx/src/modules/accounts/repositories/IUsersRepository.ts
+++ chapter-2/rentx/src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,5 +1,7 @@
 import { ICreateUserDto } from "../dtos/ICreateUserDTO";
+import { User } from "../entities/User";
 
 export interface IUsersRepository {
   create(data: ICreateUserDto): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
