3bb2ea11365891db0023605b69a3e2cc83615b70
Note45 <antonion.2048@gmail.com>
2022-06-23 14:03:49 -0300

    add implementation of findAvailable method in carsRepository

--- chapter-4/rentx/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ chapter-4/rentx/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -43,4 +43,30 @@ export class CarsRepository implements ICarsRepository {
 
     return car;
   }
+
+  async findAvailable(
+    category_id?: string,
+    brand?: string,
+    name?: string
+  ): Promise<Car[]> {
+    const carsQuery = this.repository
+      .createQueryBuilder("c")
+      .where("available = :availabe", { availabe: true });
+
+    if (brand) {
+      carsQuery.andWhere("c.brand = :brand", { brand });
+    }
+
+    if (name) {
+      carsQuery.andWhere("c.name = :name", { name });
+    }
+
+    if (category_id) {
+      carsQuery.andWhere("c.category_id = :category_id", { category_id });
+    }
+
+    const cars = await carsQuery.getMany();
+
+    return cars;
+  }
 }
